961989573b97cab62bbe103ca59d1a393dc7c690
Richie Taylor <richie.taylor@Neuro-Tick.com>
2019-02-05 16:21:23 +0000

    Preventing navigation awy if formis dirty

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/User';
 import { AlertifyService } from 'src/app/_services/alertify.service';
@@ -12,6 +12,12 @@ import { NgForm } from '@angular/forms';
 export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm: NgForm;    // This allows us to communicate to the 'editForm' form on the page
   user: User;
+  @HostListener('window:beforeunload', ['$event'])  // This adds a listener for the window beforeunloadEvent
+  unLoadNotification($event: any) { // if this is captured it can stop the page unloading, we have no control over how this happens
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
   constructor(private _route: ActivatedRoute,
               private _alertify: AlertifyService) { }
